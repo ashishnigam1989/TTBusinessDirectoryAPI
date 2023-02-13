9d78e69d9c504500194d9315ffeb14cb6e59a0a9
Umakant <rinkusngh12@gmail.com>
2023-02-14 01:37:59 +0530

    Usermanagement

--- TTBusinessAdminPanel/Controllers/UserController.cs
+++ TTBusinessAdminPanel/Controllers/UserController.cs
@@ -1,7 +1,13 @@
 ﻿using ApplicationService.IServices;
+using CommonService.RequestModel;
 using CommonService.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Text;
 
 namespace TTBusinessAdminPanel.Controllers
 {
@@ -9,10 +15,13 @@ namespace TTBusinessAdminPanel.Controllers
     {
         private readonly ILogger<UserController> _logger;
         private IAccount _account;
-        public UserController(ILogger<UserController> logger, IAccount account)
+
+        private IMaster _master;
+        public UserController(ILogger<UserController> logger, IAccount account, IMaster master)
         {
             _logger = logger;
             _account = account;
+            _master = master;
         }
 
         public IActionResult Index()
@@ -32,12 +41,54 @@ namespace TTBusinessAdminPanel.Controllers
 
         public IActionResult Add()
         {
+            BindRoles();
             return View();
         }
 
-        public IActionResult Edit()
+        [HttpPost]
+        public IActionResult Add(UserRequestModel userRequest)
         {
-            return View();
+            try
+            {
+                if (ModelState.IsValid)
+                {
+
+                    userRequest.Password = RandomPassword();
+                    userRequest.UserName = userRequest.EmailAddress;
+                    var isadded = _account.CreateUser(userRequest).Result;
+                    if (isadded)
+                    {
+                        return RedirectToAction("Index", "User");
+                    }
+                }
+                else
+                {
+                    return View();
+                }
+            }
+            catch
+            {
+                throw;
+            }
+            return RedirectToAction("Add", "User");
+        }
+
+        public IActionResult Edit(int id)
+        {
+            UserRequestModel umodel = new UserRequestModel();
+            var user = _account.GetUserById(id).Result;
+            if (user != null)
+            {
+                umodel = new UserRequestModel() {
+                    Name=user.Name,
+                    Surname=user.Surname,
+                    EmailAddress = user.EmailAddress,
+                    Mobile=user.Mobile,
+                    RoleId=user.RoleId
+                };
+            }
+            BindRoles();
+            return View("Add", umodel);
         }
 
         public IActionResult Approve()
@@ -48,6 +99,41 @@ namespace TTBusinessAdminPanel.Controllers
         public IActionResult RoleMenuMap()
         {
             return View();
+        }
+
+        private void BindRoles()
+        {
+            var r = _master.GetRoles();
+            ViewBag.Roles = new SelectList(r, "Id", "DisplayName");
+        }
+
+        private string RandomString(int size, bool lowerCase)
+        {
+            StringBuilder builder = new StringBuilder();
+            Random random = new Random();
+            char ch;
+            for (int i = 0; i < size; i++)
+            {
+                ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                builder.Append(ch);
+            }
+            if (lowerCase)
+                return builder.ToString().ToLower();
+            return builder.ToString();
+        }
+
+        private int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+        private string RandomPassword()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(RandomString(2, true));
+            builder.Append(RandomNumber(0, 9));
+            builder.Append(RandomString(2, false));
+            return builder.ToString();
         }
     }
 }
